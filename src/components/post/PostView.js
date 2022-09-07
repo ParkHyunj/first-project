6ff05877372754fcefa214af7f2fed9b0078127b
hyunjin <phj97211@naver.com>
2022-09-07 23:00:15 +0900

    ADD: data로 치환해서 array는 받아옴. 이거를 for화, 함수화 시켜야 한다.

--- src/components/post/PostView.js
+++ src/components/post/PostView.js
@@ -1,65 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import PostNo from './data.json';
 import './Post.css';
-import { useParams } from 'react-router-dom';
 
-function PostView(props) {
+function PostView() {
   
-  let { no } = useParams();
-  let [data] = props.datas(no);
+  const [ data, setData ] = useState({});
   
+  useEffect(() => {
+     setData(PostNo.datas[""]);
+   }, []);
+   
   return (
     <div>
-      <h4>{data.no}</h4>
-      <p>{data.title}</p>
-      <p>{data.content}</p>
+      <h2 align="center">게시글 상세정보</h2>
+
+      <div className="post-view-wrapper">
+        {
+          data ? ( 
+            <div>
+              <div className="post-view-row">
+                <label>게시글 번호</label>
+                <label>{ data.no }</label>
+              </div>
+              <div className="post-view-row">
+                <label>제목</label>
+                <label>{ data.title }</label>
+              </div>
+              <div className="post-view-row">
+                <label>작성일</label>
+                <label>{ data.createDate }</label>
+              </div>
+              <div className="post-view-row">
+                <label>조회수</label>
+                <label>{ data.readCount }</label>
+              </div>
+              <div className="post-view-row">
+                <label>내용</label>
+                <div>
+                  { data.content }
+                </div>
+              </div>
+            </div>
+           ) : ''
+        }
+      </div>  
     </div>
-  );
-
-
-
-  // const [ data, setData ] = useState({});
-  
-  // useEffect(() => {
-  //    setData(PostNo.datas.no);
-  //  }, [ ]);
-
-  // return (
-  //   <div>
-  //     <h2 align="center">게시글 상세정보</h2>
-
-  //     <div className="post-view-wrapper">
-  //       {
-  //         data ? (
-  //           <div>
-  //             <div className="post-view-row">
-  //               <label>게시글 번호</label>
-  //               <label>{ data.no }</label>
-  //             </div>
-  //             <div className="post-view-row">
-  //               <label>제목</label>
-  //               <label>{ data.title }</label>
-  //             </div>
-  //             <div className="post-view-row">
-  //               <label>작성일</label>
-  //               <label>{ data.createDate }</label>
-  //             </div>
-  //             <div className="post-view-row">
-  //               <label>조회수</label>
-  //               <label>{ data.readCount }</label>
-  //             </div>
-  //             <div className="post-view-row">
-  //               <label>내용</label>
-  //               <div>
-  //                 {data.content}
-  //               </div>
-  //             </div>
-  //           </div>
-  //         ) : ''
-  //       }
-  //     </div>
-  //   </div>
-  // )
+  )
 }
 
 export default PostView;
