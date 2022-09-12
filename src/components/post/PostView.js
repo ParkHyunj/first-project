d0fab59810bf700e6c101c960adba55f0cd33ba1
hyunjin <phj97211@naver.com>
2022-09-12 20:35:04 +0900

    ADD : f12 오류는 없으나 실행 안됨.

--- src/components/post/PostView.js
+++ src/components/post/PostView.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PostNo from './data.json';
 import './Post.css';
+import { useParams } from "react-router-dom";
 
 function PostView() {
   
@@ -9,43 +10,20 @@ function PostView() {
   useEffect(() => {
      setData(PostNo.datas[""]);
    }, []);
+
+   const { day } = useParams();
+   const dataList = PostNo.datas.filter(data => data.no === Number(day));
    
   return (
     <div>
-      <h2 align="center">게시글 상세정보</h2>
-
-      <div className="post-view-wrapper">
-        {
-          data ? ( 
-            <div>
-              <div className="post-view-row">
-                <label>게시글 번호</label>
-                <label>{ data.no }</label>
-              </div>
-              <div className="post-view-row">
-                <label>제목</label>
-                <label>{ data.title }</label>
-              </div>
-              <div className="post-view-row">
-                <label>작성일</label>
-                <label>{ data.createDate }</label>
-              </div>
-              <div className="post-view-row">
-                <label>조회수</label>
-                <label>{ data.readCount }</label>
-              </div>
-              <div className="post-view-row">
-                <label>내용</label>
-                <div>
-                  { data.content }
-                </div>
-              </div>
-            </div>
-           ) : ''
-        }
-      </div>  
-    </div>
-  )
+      { dataList.map(data => (
+          <div key={data.no}>
+            <label>{data.no}</label>
+            <label>{data.title}</label>
+          </div>
+    ))}
+  </div>
+  );
 }
 
 export default PostView;
