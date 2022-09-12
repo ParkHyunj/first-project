ec3e37fe70e1148acfc92292b5182155873464da
hyunjin <phj97211@naver.com>
2022-09-12 20:59:27 +0900

    ADD: 다시 시도

--- src/components/post/PostView.js
+++ src/components/post/PostView.js
@@ -6,23 +6,47 @@ import { useParams } from "react-router-dom";
 function PostView() {
   
   const [ data, setData ] = useState({});
-  
-  useEffect(() => {
-     setData(PostNo.datas[""]);
-   }, []);
 
-   const { day } = useParams();
-   const dataList = PostNo.datas.filter(data => data.no === Number(day));
-   
+  useEffect(() => {
+    setData(PostNo.datas);
+  }, [ ]);
+
   return (
     <div>
-      { dataList.map(data => (
-          <div key={data.no}>
-            <label>{data.no}</label>
-            <label>{data.title}</label>
+      <h2 align="center">게시글 상세정보</h2>
+
+      <div className="post-view-wrapper">
+        {
+          data ? (
+        <>
+          <div className="post-view-row">
+           <label>게시글 번호</label>
+           <label>{ data.no }</label>
           </div>
-    ))}
-  </div>
+          <div className="post-view-row">
+           <label>제목</label>
+            <label>{ data.title }</label>
+          </div>
+          <div className="post-view-row">
+           <label>작성일</label>
+           <label>{ data.createDate }</label>
+          </div>
+          <div className="post-view-row">
+            <label>조회수</label>
+            <label>{ data.readCount }</label>
+          </div>
+          <div className="post-view-row">
+            <label>내용</label>
+            <div> { data.content } </div>
+          </div>
+        </>
+            ) : '해당 게시글을 찾을 수 없습니다.'
+           }
+          <button className="post-view-go-list-btn">
+            목록으로 돌아가기
+          </button>
+      </div>
+    </div>
   );
 }
 
