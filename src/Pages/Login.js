6d1700f63a8d08fa7b2e75763b22252c477fe28e
hyunjin <phj97211@naver.com>
2022-09-15 17:57:47 +0900

    ADD: login 구현, 내가 한 코딩이 아닌 reactbootstrap 이용

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,17 +1,30 @@
 import React from "react";
-import "../components/write/Writing.css";
+// import "../components/write/Writing.css";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Form';
 
 function Login() {
     return (
-        <div className='Writing'>
-        <input 
-            type='text' id='title_txt' placeholder='email'/>
-        <input 
-            type='text' id='title_txt' placeholder='password'/>
-        <p>
-            <button>로그인</button>
-        </p>
-    </div>
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="자동 로그인 활성" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          <button>Login</button>
+        </Button>
+      </Form>
     );
 }
 
