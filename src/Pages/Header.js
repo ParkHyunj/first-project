bc84002d3925ef2cc0f760be8267d539b2726b72
hyunjin <phj97211@naver.com>
2022-09-17 16:19:56 +0900

    ADD: flex css, css를 이용하여, style / 부모*자식 관계는 바로 직속만 해당

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,33 +1,59 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import "../pages/Header.css";
 
 function Header(){
     return(
-        <div className="container">
-            <img src= "/img/img.png" alt="profile"/>
-          <div className="item">
-            <Link to="/exercise" style={{ textDecoration: "none"}}>
-                운동
-            </Link>
+          <div className="whole">
+            <Container>
+              <div className="menu-header">
+                
+                  <div className="logo-wrap">
+                    <div className="logo1-wrap">
+                      <div className="logo">
+                        <img src= "/img/img.png" alt="profile"/>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div className="menu-wrap">
+                    <div className="menu1-wrap">
+                      <div className="menu1">
+                        <Link to="/exercise" style={{ textDecoration: "none"}}>
+                            운동
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="menu1-wrap">
+                      <div className="menu2"> 
+                        <Link to="/cook" style={{ textDecoration: "none"}}>
+                            요리
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="menu1-wrap">
+                      <div className="menu3"> 
+                        <Link to="/game" style={{ textDecoration: "none"}}>
+                            게임
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="login-wrap">
+                    <div className="login1-wrap">
+                      <div className="login"> 
+                        <Link to="/login" style={{ textDecoration: "none"}}>
+                          <Button variant="primary">Login</Button>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+              
+              </div>
+            </Container>
           </div>
-          <div className="item">
-            <Link to="/cook" style={{ textDecoration: "none"}}>
-                요리
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="/game" style={{ textDecoration: "none"}}>
-                게임
-            </Link>
-          </div>
-          <div className="item">
-            <Link to="/login" style={{ textDecoration: "none"}}>
-                <Button variant="primary">Login</Button>
-           </Link>
-          </div>  
-        </div>
     );
 }
 
