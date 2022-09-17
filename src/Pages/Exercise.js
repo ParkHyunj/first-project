4b7e500242aac260253817cd71e84138fdb52fc9
hyunjin <phj97211@naver.com>
2022-09-17 16:48:50 +0900

    ADD: 게시판 페이지 css

--- src/Pages/Exercise.js
+++ src/Pages/Exercise.js
@@ -3,21 +3,35 @@ import { Link } from "react-router-dom";
 import PostMain from '../components/post/PostMain';
 import Header from '../pages/Header';
 import '../pages/Exercise.css';
+import { Container } from 'react-bootstrap';
+
 
 function Exercise() {
     return (
-        <div className="Exercise"> 
-            <div className="item">
-                <Header/>
-            </div>
-            <div className="item">
-                <PostMain />
-            </div>
-            <div className="item">
-                <Link to = "/exercise/writing">
-                    <button>글쓰기</button>
-                </Link>
-            </div>
+        <div className="exercise"> 
+            <Container>
+                
+                <div className="exercise-wrap">
+                    <div className="option1-wrap">
+                        <div className="option1">
+                            <Header/>
+                        </div>
+                    </div>
+                    <div className="option1-wrap">
+                        <div className="option2">
+                            <PostMain />
+                        </div>
+                    </div>
+                    <div className="option1-wrap">
+                        <div className="option3">
+                            <Link to = "/exercise/writing">
+                                <button>글쓰기</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+           
+            </Container>
         </div>
     );
 }
