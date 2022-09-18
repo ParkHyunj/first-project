01ec1e772a4b31c63e152e6e93a2ddc93b2746b4
hyunjin <phj97211@naver.com>
2022-09-18 14:39:37 +0900

    ADD: css 수정

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -5,57 +5,57 @@ import "../pages/Header.css";
 import logo from '../assets/img.png';
 
 function Header(){
-    return(
-          <div className="whole">
-            <Container>
-              <div className="menu-header">
-                
-                  <div className="logo-wrap">
-                    <div className="logo1-wrap">
-                      <div className="logo">
-                        <img src= {logo} alt="profile"/>
-                      </div>
-                    </div>
-                  </div>
-                  
-                  <div className="menu-wrap">
-                    <div className="menu1-wrap">
-                      <div className="menu1">
-                        <Link to="/exercise" style={{ textDecoration: "none"}}>
-                            운동
-                        </Link>
-                      </div>
-                    </div>
-                    <div className="menu1-wrap">
-                      <div className="menu2"> 
-                        <Link to="/cook" style={{ textDecoration: "none"}}>
-                            요리
-                        </Link>
-                      </div>
-                    </div>
-                    <div className="menu1-wrap">
-                      <div className="menu3"> 
-                        <Link to="/game" style={{ textDecoration: "none"}}>
-                            게임
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="login-wrap">
-                    <div className="login1-wrap">
-                      <div className="login"> 
-                        <Link to="/login" style={{ textDecoration: "none"}}>
-                          <Button variant="primary">Login</Button>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-              
+  return(
+    <div className="whole">
+      <Container>
+        <div className="menu-header">
+          
+            <div className="logo-wrap">
+              <div className="logo1-wrap">
+                <div className="logo">
+                  <img src={logo} alt="logo"/>
+                </div>
               </div>
-            </Container>
-          </div>
-    );
+            </div>
+            
+            <div className="menu-wrap">
+              <div className="menu1-wrap">
+                <div className="menu1">
+                  <Link to="/exercise" className="menu1_link">
+                      <p>운동</p>
+                  </Link>
+                </div>
+              </div>
+              <div className="menu2-wrap">
+                <div className="menu2"> 
+                  <Link to="/cook" className="menu2_link">
+                      <p>요리</p>
+                  </Link>
+                </div>
+              </div>
+              <div className="menu3-wrap">
+                <div className="menu3"> 
+                  <Link to="/game" className="menu3_link">
+                      <p>게임</p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="login-wrap">
+              <div className="login1-wrap">
+                <div className="login"> 
+                  <Link to="/login" className="login_link">
+                    <p  style={{ color: "#B2DAFB", fontWeight: "700" }}>Login</p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+        
+        </div>
+      </Container>
+    </div>
+);
 }
 
 export default Header;
