739617e7503f390522489616941956cf9257fa4d
hyunjin <phj97211@naver.com>
2022-10-01 15:18:32 +0900

    DOCS: login 입력한 데이터 검증 구현

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,100 +1,77 @@
 import React, { useState } from "react";
-import Header from '../pages/Header';
+import '../pages/Login.css';
 
 function Login() {
-  const [ id, setId ] = useState('');
-  const [ pw, setPw ] = useState('');
 
-  const [ button, setButton ] = useState(true);
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  function changeButton() {
-    id.includes('@') && pw.length >= 8 ? setButton(false) : setButton(true);
-  }
+  const database = [
+    {
+      username: "hyunjin@gmail.com",
+      password: "pass1234"
+    },
+    {
+      username: "hyunjin@naver.com",
+      password: "pass5678"
+    }
+  ];
 
-  const realId = "hyunjin@gmail.com";
-  const realPw = "12345678";
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const { uname, pass } = document.forms[0];
+    const userData = database.find((user) => user.username === uname.value);
+
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Email </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
 
   return (
-    <div>
-      <input
-        placeholder="Your Email"
-        id="id"
-        className="login"
-        onChange={e => { 
-          setId(e.target.value);
-        }}
-        onKeyUp={ changeButton }
-      />
-      <input
-        type="password"
-        placeholder="Your Password"
-        id="password"
-        className="login"
-        onChange={e => {
-          setPw(e.target.value)
-        }}
-        onKeyUp={ changeButton }
-      />
-      <button
-        type = "button"
-        className = "loginButton"
-        disabled={button}
-        onClick={e => {
-          if (realId == id) {
-            if (realPw == pw) {
-              e.stopPropagation();
-              Header();
-            }
-          } else {
-            alert("아이디 혹은 비밀번호가 일치하지 않습니다.");
-          }
-        }}
-      >
-        button</button>
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Login</div>
+        {isSubmitted ? <div>Login Success!</div> : renderForm}
+      </div>
     </div>
   );
 }
 
 export default Login;
-
-
-
-
-
-// import React from "react";
-// import Form from 'react-bootstrap/Form';
-// import Button from 'react-bootstrap/Form';
-// import '../pages/Login.css';
-// import { Container } from 'react-bootstrap';
-
-// function Login() {
-//     return (
-//       <div>
-//         <Container>
-//           <Form>
-//             <Form.Group className="mb-3" controlId="formBasicEmail">
-//               <Form.Label>Email address</Form.Label>
-//               <Form.Control type="email" placeholder="Enter email" />
-//               <Form.Text className="text-muted">
-//                 We'll never share your email with anyone else.
-//               </Form.Text>
-//             </Form.Group>
-  
-//             <Form.Group className="mb-3" controlId="formBasicPassword">
-//               <Form.Label>Password</Form.Label>
-//               <Form.Control type="password" placeholder="Password" />
-//             </Form.Group>
-//             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-//               <Form.Check type="checkbox" label="자동 로그인 활성" />
-//             </Form.Group>
-//             <Button variant="primary" type="submit">
-//               <button>Login</button>
-//             </Button>
-//           </Form>
-//         </Container>
-//       </div>
-//     );
-// }
-
-// export default Login;
-
